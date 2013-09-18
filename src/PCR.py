39e2e5020c3b2d2ceef5a4c65b4f9dff7b879f94
cacdoodle <cac140@zips.uakron.edu>
2013-09-18 19:48:30 -0400

    PCR cycles successfully
    Interestingly, because it doesn't remove the initial strand how I have
    set it up, each cycle raises the fragments by a power of 3. Perhaps a
    third nested loop could be implemented to remove used fragments?

--- src/PCR.py
+++ src/PCR.py
@@ -5,7 +5,7 @@ cycles = 1
 dropOffRate = 0
 primerSize = 20
 dnaTemplateSize=2000
-
+segment=[0,0]
 dna = RandomDna().randDna(dnaTemplateSize)
 primer = RandomDna().randPrimer(dna, primerSize, segmentSize)
 generatedDna = [dna]
@@ -19,11 +19,15 @@ print "5'",dna[0], "3'"
 print "3'", dna[1], "5'"
 p2 = dna[1].index(primer[1])-1
 print " "*p2, "3'", primer[1],"5'"
-segment = [dna[0][p1:p2+len(primer)],reverse(dna[1][p1:p2+len(primer)]]
-
+segment[0] = dna[0][p1:p2+len(primer)]
+segment[1] = dna[1][p1:p2+len(primer)]
+print segment
 for x in xrange(cycles):
   break
-#for x in xrange(cycles):
-  #for y in xrange(len(generatedDna)):
-    #newCopy = pcrCycle(dna,segment)
-    #generatedDna += newCopy
+for x in xrange(cycles):
+  #listLength=len(generatedDna)
+  for y in xrange(2):
+    toProcess=generatedDna(y)
+    newCopy = RandomDna().pcrCycle(toProcess[0],toProcess[1],segment[0],segment[1])
+    generatedDna += newCopy
+print generatedDna

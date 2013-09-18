fd15609fbd532fb10baf7d4361d31ada25e7d403
clc89 <clc89@zips.uakron.edu>
2013-09-18 01:30:24 -0400

    created reverse complment method

--- src/PCR.py
+++ src/PCR.py
@@ -1,7 +1,15 @@
 from RandomDnaTemplate import RandomDna
 
-
-dna = RandomDna().randDna(20)
+dna = RandomDna().randDna(10)
 primer = RandomDna().randPrimer(dna, 5)
+cycles = 1
+dropOffRate = 0
+i = 1;
+generatedDna = []
+while i <= cycles:
+    print "cycle number: ", i
+    i+=1
+
 
 print dna
+print primer
--- src/RandomDnaTemplate.py
+++ src/RandomDnaTemplate.py
@@ -12,21 +12,34 @@ class RandomDna(object):
         firstStrand=''
         for x in xrange(n):
             firstStrand=firstStrand+base[randrange(4)]
-        #print firstStrand
-        secondStrand=firstStrand.replace('A','t')
+            
+        secondStrand = self.reverseComplement(firstStrand)
+
+        dna[0]=firstStrand
+        dna[1]=secondStrand
+        
+        return dna
+    
+    # select a random primer
+    # based off of the length and the dna template given.
+    def randPrimer(self, dna, length):
+        start = randrange(length)
+        primer = [0,0]
+        primer[0] = dna[0][start:start+length]
+        primer[1] = self.reverseComplement(primer[0])
+        
+        return primer
+    
+    
+    
+    def reverseComplement(self, dna):
+        secondStrand=dna.replace('A','t')
         secondStrand=secondStrand.replace('T','a')
         secondStrand=secondStrand.replace('C','g')
         secondStrand=secondStrand.replace('G','c')
         secondStrand=secondStrand.upper()
-        #print secondStrand
-        dna[0]=firstStrand
-        dna[1]=secondStrand[::-1]
-        
-        return dna
-    
-    #TODO - write code to select a random primer
-    #       based off of the length and the dna template given.
-    def randPrimer(self, dna, length):
-        return "testPrimer"
+        secondStrand = secondStrand[::-1] 
+               
+        return secondStrand
 
 
